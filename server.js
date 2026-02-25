b48e28eae17381c930600863c27c2a082e6677b0
smartoolinbox90-wq <smartoolinbox90@gmail.com>
2026-02-25 06:40:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
+const path = require("path");
 
-app.use(cors());
-app.use(express.json());
+app.use(express.static(__dirname));
 
-app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
-});
-
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running");
 });
